71f826b70fae0ffa2f1a557801f5762d78f261c9
Nikhil-RT <nraviteja403@msitprogram.net>
2020-01-08 21:44:45 +0530

    comit pro

---
+++ Day - 2/Digraph/Digraph.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Digraph {
+    int V; // no of vertices 
+    int E; //no of edges
+    Bag<Integer>[] adj;
+    int[] indegree;
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    public void addEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(w);
+        indegree[w]++;
+        E++;
+    }
+
+    public Iterable <Integer> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    public static void main(String[] args) {
+        Digraph graph = new Digraph();
+    }
+}
---
+++ Day - 2/Digraph/Wordnet.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+// import java.util.*;
+class Wordnet {
+    HashMap<Integer, List<String>> synsets;
+    HashMap<Integer, List<Integer>> hypernyms;
+    public Wordnet(String Synsets, String Hypernyms) throws IOException {
+        // ParseSynsets(Synsets);
+        ParseHypernyms(Hypernyms);
+    }
+    private HashMap<Integer, List<String>> ParseSynsets(String Synsets) throws IOException {
+        FileReader fr1 = new FileReader("C:\\Users\\Nikhil\\Desktop\\Msit\\ADS2\\Day - 1\\wordnet\\synsets.txt");
+        synsets = new HashMap<>(); 
+        BufferedReader br1 = new BufferedReader(fr1);
+        String i;
+        while ((i = br1.readLine()) != null) {
+            List<String> temp = new ArrayList<String>();
+            String s[] = i.split(",");
+            // System.out.println(s[0]);
+            for(int j = 1; j < s.length; j++) {
+                temp.add(s[j]);
+                synsets.put(Integer.parseInt(s[0]), temp);
+            }    
+        }
+        System.out.println("------------------------------------------------------------------------------------------");
+        br1.close();
+        for(int k : synsets.keySet())
+        System.out.println(k);
+        return synsets;
+    }
+
+    private HashMap<Integer, List<Integer>> ParseHypernyms(String Hypernyms) throws IOException {
+        FileReader fr2 = new FileReader("C:\\Users\\Nikhil\\Desktop\\Msit\\ADS2\\Day - 1\\wordnet\\hypernyms.txt");
+        hypernyms = new HashMap<>();
+        BufferedReader br2 = new BufferedReader(fr2);
+        String j;
+        while ((j = br2.readLine()) != null) {
+            List<Integer> temp = new ArrayList<Integer>();
+            String h[] = j.split(",");
+            // System.out.println(h[0]);
+            for(int i = 1; i < h.length; i++) {
+                temp.add(Integer.parseInt(h[i]));
+                hypernyms.put(Integer.parseInt(h[0]), temp);
+            }
+        }
+        br2.close();
+        for(int k : hypernyms.keySet()) 
+            System.out.println(k + "  " + hypernyms.get(k).toString());
+        return hypernyms;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Wordnet obj = new Wordnet("Synsets","Hypernyms");
+    }
+}
